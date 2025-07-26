8920d5b3f6b426bd5d867123539c4379cee672eb
Jan Wennrich <42703348+JanWennrich@users.noreply.github.com>
2025-07-26 19:12:28 +0200

    feat: add "GeneratorCommand"
    This command downloads the data from BGG and generates the HTML

---
+++ src/Command/GeneratorCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JanWennrich\BoardGames\Command;
+
+use JanWennrich\BoardGames\HtmlGeneratorInterface;
+use JanWennrich\BoardGames\OwnedBoardgamesLoaderInterface;
+use JanWennrich\BoardGames\PlayedBoardgamesLoaderInterface;
+use JanWennrich\BoardGames\WishlistedBoardgamesLoaderInterface;
+use Symfony\Component\Console\Attribute\Argument;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(name: 'generate')]
+final class GeneratorCommand extends Command
+{
+    public function __construct(
+        private readonly WishlistedBoardgamesLoaderInterface $wishlistedBoardgamesLoader,
+        private readonly PlayedBoardgamesLoaderInterface $playedBoardgamesLoader,
+        private readonly OwnedBoardgamesLoaderInterface $ownedBoardgamesLoader,
+        private readonly HtmlGeneratorInterface $htmlGenerator,
+    ) {
+        parent::__construct();
+    }
+
+    public function __invoke(
+        SymfonyStyle $io,
+        #[Argument] string $bggUsername,
+        #[Argument] string $buildDirectory = __DIR__ . '/../../public',
+    ): int {
+        $wishlistedBoardgames = $this->wishlistedBoardgamesLoader->getForUser($bggUsername);
+        $playedBoardgames = $this->playedBoardgamesLoader->getForUser($bggUsername);
+        $ownedBoardgames = $this->ownedBoardgamesLoader->getForUser($bggUsername);
+
+        $html = $this->htmlGenerator->generateHtml(
+            $ownedBoardgames,
+            $playedBoardgames,
+            $wishlistedBoardgames,
+            $bggUsername,
+        );
+
+        if (!is_dir($buildDirectory)) {
+            mkdir($buildDirectory);
+        }
+
+        file_put_contents("$buildDirectory/index.html", $html);
+
+        return Command::SUCCESS;
+    }
+}
