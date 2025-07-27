8781c2f49124ecec6276948b27fef6a314e972f1
Jan Wennrich <42703348+JanWennrich@users.noreply.github.com>
2025-07-27 22:16:53 +0200

    feat(GeneratorCommand): add CLI info output

--- src/Command/GeneratorCommand.php
+++ src/Command/GeneratorCommand.php
@@ -27,9 +27,19 @@ final class GeneratorCommand extends Command
 
     public function __invoke(
         SymfonyStyle $io,
-        #[Argument] string $bggUsername
+        #[Argument] string $bggUsername,
     ): int {
+        $io->title("BoardGameGeek Portfolio Generator");
+
         $buildDirectory = __DIR__ . '/../../public';
+
+        $io->info(
+            sprintf(
+                "Generating portfolio for BGG user '%s' into '%s'…",
+                $bggUsername,
+                realpath($buildDirectory),
+            ),
+        );
 
         if (!is_dir($buildDirectory) && !@mkdir($buildDirectory)) {
             $io->error(sprintf('Build directory "%s" could not be created', $buildDirectory));
@@ -37,8 +47,13 @@ final class GeneratorCommand extends Command
             return Command::FAILURE;
         }
 
+        $io->info('Querying wishlisted boardgames…');
         $wishlistedBoardgames = $this->wishlistedBoardgamesLoader->getForUser($bggUsername);
+
+        $io->info('Querying played boardgames…');
         $playedBoardgames = $this->playedBoardgamesLoader->getForUser($bggUsername);
+
+        $io->info('Querying owned boardgames…');
         $ownedBoardgames = $this->ownedBoardgamesLoader->getForUser($bggUsername);
 
         $html = $this->htmlGenerator->generateHtml(
@@ -47,8 +62,10 @@ final class GeneratorCommand extends Command
             $wishlistedBoardgames,
             $bggUsername,
         );
+        $io->info("Generated HTML from queried data");
 
         file_put_contents("$buildDirectory/index.html", $html);
+        $io->success("Portfolio successfully created in '$buildDirectory'");
 
         return Command::SUCCESS;
     }
